ece8fbec89e4dd44a0651558005c0c71686ce246
Thorantt <86085299+Thorantt@users.noreply.github.com>
2023-04-07 12:11:05 -0400

    eh

--- tp3.c
+++ tp3.c
@@ -244,29 +244,6 @@ void SaveImagePgm(char* bruit,char* name,float** mat,int lgth,int wdth)
 // PROGRAMME PRINCIPAL -------------------------------------
 //----------------------------------------------------------
 //----------------------------------------------------------
-
-float rec(float* mat, int length)
- {
-  if (length==1){
-    return mat[0];
-  }
-  else if (length==2){
-    return mat[0]+mat[1];
-  }
-  int l1=length/2;
-  int l2=length-l1;
-  float* mat1=new float[l1];
-  for(int i=0;i<l1;i++){
-    mat1[i]=mat[i];
-  }
-  float* mat2=new float[l2];
-  for(int i=0;i<l2;i++){
-    mat2[i]=mat[l1+i];
-  }
-  return rec(mat1,l1) + rec(mat2,l2);
- }
-
-
 int main(int argc,char** argv)
 {
  int   i,j,k,l;
@@ -305,7 +282,17 @@ int main(int argc,char** argv)
  if (argc>1)  { NbInt=atoi(argv[1]); }
  float* VctPts=fmatrix_allocate_1d(NbInt+1);
 
- 
+ float rec(float* mat, int length){
+  if (length==1){
+    return mat[0]
+  }
+  else if (length==2){
+    return mat[0]+mat[1]
+  }
+  int l1=length/2
+  int l2=length-l1
+  return rec(mat[0:l1],l1) + rec(mat[l1+1:],l2)
+ }
  //Programmer ici
  
  
